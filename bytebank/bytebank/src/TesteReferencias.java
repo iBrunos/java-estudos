2030683058ddfeb019d464ffd8aa0128886bdd54
iBrunos <seixasbru@gmail.com>
2022-10-11 13:02:59 -0300

    Consertando projeto

--- bytebank/bytebank/src/TesteReferencias.java
+++
@@ -1,8 +0,0 @@
-public class TesteReferencias {
-    public static void main() {
-        Conta primeiraConta = new Conta();
-        primeiraConta.saldo = 300;
-
-        System.out.println("Saldo da primeira: " + primeiraConta.saldo);
-    }
-}
