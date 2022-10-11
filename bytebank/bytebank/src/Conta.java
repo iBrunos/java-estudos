ff06e837f1aee3a6b60853ff3e8589b85d83c11c
iBrunos <seixasbru@gmail.com>
2022-10-11 12:08:19 -0300

    criando metodos

--- bytebank/bytebank/src/Conta.java
+++ bytebank/bytebank/src/Conta.java
@@ -5,4 +5,19 @@ public class Conta {
     int numero;
     String titular;  
 
-    }
+    void deposita(double valor){
+        this.saldo = this.saldo + valor;
+
+    };
+
+    public boolean saca(double valor) {
+
+        if (this.saldo >= valor){
+                this.saldo = this.saldo - valor;
+                return true;
+        } else {
+            return false;
+        }
+    };
+
+}
