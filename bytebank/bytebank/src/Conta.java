a8a8630bbad0ae11e3dbd2ec0d015976071fb080
iBrunos <seixasbru@gmail.com>
2022-10-11 11:35:58 -0300

    Class conta

---
+++ bytebank/bytebank/src/Conta.java
@@ -0,0 +1,10 @@
+public class Conta {
+    public static void main(String[] args) {
+        
+        class Conta {
+            double salto;
+            int agencia;
+            int numero;
+            String titular; 
+        }
+}
