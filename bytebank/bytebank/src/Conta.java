033a69290198c573a50cd64378fa839c755cad0e
iBrunos <seixasbru@gmail.com>
2022-10-11 12:29:47 -0300

    Consertando arquivos

--- bytebank/bytebank/src/Conta.java
+++ bytebank/bytebank/src/Conta.java
@@ -1,34 +1,29 @@
 public class Conta {
-    
-    double saldo;
-    int agencia = 42;
-    int numero;
-    Cliente titular; 
-    
 
-    void deposita(double valor){
-        this.saldo = this.saldo + valor;
+	double saldo;
+	int agencia;
+	int numero;
+	Cliente titular;
 
-    };
+	public void deposita(double valor) {
+		this.saldo = this.saldo + valor;
+	}
 
-    public boolean saca(double valor) {
+	public boolean saca(double valor) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			return true;
+		} else {
+			return false;
+		}
+	}
 
-        if (this.saldo >= valor){
-                this.saldo = this.saldo - valor;
-                return true;
-        } else {
-            return false;
-        }
-    };
-
-    public boolean tranfere(double valor, Conta destino){
-        if (this.saldo >= valor){
-            this.saldo -= valor;
-            destino.deposita(valor);
-            return true;
-        };
-        return false;
-    }
-
-
-};
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.deposita(valor);
+			return true;
+		}
+		return false;
+	}
+}
--- bytebank/bytebank/src/CriaConta.java
+++ bytebank/bytebank/src/CriaConta.java
@@ -14,5 +14,7 @@ public class CriaConta {
         System.out.println("primeira conta tem " + primeiraConta.saldo);
         System.out.println("segunda conta tem " + segundaConta.saldo);
 
+        System.out.println("referenciar: " + primeiraConta);
     }
+
 }
