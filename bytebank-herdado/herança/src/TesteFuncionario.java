1b3012bfc32d29da74b8d80f95af25d5fc62d06a
iBrunos <seixasbru@gmail.com>
2022-10-19 10:56:16 -0300

    Criando funcionário

--- bytebank-herdado/herança/src/TesteFuncionario.java
+++ bytebank-herdado/herança/src/TesteFuncionario.java
@@ -1,5 +1,9 @@
 public class TesteFuncionario {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        Funcionario nico = new Funcionario();
+        nico.setNome("Bruno Seixas");
+        nico.setCpf("223355646-9");
+        nico.setSalario(2600.00);
     }
 }
