39fad91143557ec0b4fe4b947a768df740c9a5e7
iBrunos <seixasbru@gmail.com>
2022-10-25 22:05:50 -0300

    java jdbc

---
+++ conexao/loja-virtual/src/TestaConexao.java
@@ -0,0 +1,10 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class TestaConexao {
+    public static void main(String[] args) throws Exception {
+       Connetion Connection = Driver.Manager.getConnection();
+       
+    }
+}
